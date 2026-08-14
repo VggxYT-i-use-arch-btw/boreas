4edab4009ff2f462f35fd66c084d312cd1523906
VggxYT-i-use-arch-btw <viniciusgamergomes0123@gmail.com>
2026-08-14 20:39:41 -0300

    Change backend URL in backend-config.js
    Updated the backend URL for the application.

--- backend-config.js
+++ backend-config.js
@@ -1,3 +1,3 @@
 // Configuração compartilhada pelo front-end e pelo Service Worker.
 // Atualize a URL do backend somente aqui quando o túnel mudar.
-globalThis.BOREAS_BACKEND_URL = "https://notebooks-blvd-advancement-caps.trycloudflare.com";
+globalThis.BOREAS_BACKEND_URL = "https://intervals-buses-yacht-hawaiian.trycloudflare.com";
