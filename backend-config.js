d6821eeb52f6d69be36025ae97c772accb77ce27
VggxYT-i-use-arch-btw <viniciusgamergomes0123@gmail.com>
2026-08-16 01:33:30 -0300

    Update backend-config.js

--- backend-config.js
+++ backend-config.js
@@ -1,3 +1,3 @@
 // Configuração compartilhada pelo front-end e pelo Service Worker.
 // Atualize a URL do backend somente aqui quando o túnel mudar.
-globalThis.BOREAS_BACKEND_URL = "https://notebooks-blvd-advancement-caps.trycloudflare.com";
+globalThis.BOREAS_BACKEND_URL = "https://worked-manga-scenarios-overall.trycloudflare.com";
