dc27aade499ec11ed20ba9b144c158db00d60490
VggxYT-i-use-arch-btw <viniciusgamergomes0123@gmail.com>
2026-08-18 18:34:40 -0300

    Add files via upload

--- backend-config.js
+++ backend-config.js
@@ -1,1 +1,3 @@
-globalThis.BOREAS_BACKEND_URL = "https://legislature-satisfied-admission-islamic.trycloudflare.com";
+// Configuração compartilhada pelo front-end e pelo Service Worker.
+// Atualize a URL do backend somente aqui quando o túnel mudar.
+globalThis.BOREAS_BACKEND_URL = "https://into-cowboy-teaching-dot.trycloudflare.com";
