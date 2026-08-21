0cb4817ca91a289a62864932dbff9acc8389c230
VggxYT-i-use-arch-btw <viniciusgamergomes0123@gmail.com>
2026-08-21 17:59:14 -0300

    Update BOREAS_BACKEND_URL for backend configuration

--- backend-config.js
+++ backend-config.js
@@ -1,3 +1,3 @@
 // Cloudflared
 // Tunnel
-globalThis.BOREAS_BACKEND_URL = "https://viii-statement-based-arrival.trycloudflare.com";
+globalThis.BOREAS_BACKEND_URL = "https://bath-diesel-characthers-contents.trycloudflare.com";
