39c520de952c1eaec4be135769649e0331e5d02e
VggxYT-i-use-arch-btw <viniciusgamergomes0123@gmail.com>
2026-08-23 20:42:28 -0300

    Update BOREAS_BACKEND_URL to Cloudflare tunnel

--- backend-config.js
+++ backend-config.js
@@ -1,7 +1,3 @@
-const _boreasOrigin = globalThis.location?.origin || "";
-Object.defineProperty(globalThis, "BOREAS_BACKEND_URL", {
-  value: _boreasOrigin,
-  writable: false,
-  configurable: false,
-  enumerable: true,
-});
+// Cloudflared
+// Tunnel
+globalThis.BOREAS_BACKEND_URL = "https://watching-ottawa-satisfaction-citation.trycloudflare.com";
