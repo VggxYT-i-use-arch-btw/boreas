fb6ff8340301b9939232f0523499dc40a14c260a
VggxYT-i-use-arch-btw <viniciusgamergomes0123@gmail.com>
2026-08-31 18:50:36 -0300

    Add files via upload

--- attachments-images.js
+++ attachments-images.js
@@ -380,7 +380,7 @@ function renderPreviewThumbs() {
     img.addEventListener("click", e => { e.stopPropagation(); openLightbox(b64); });
     const rm = document.createElement("div");
     rm.className = "thumb-remove";
-    rm.textContent = "✕";
+    rm.innerHTML = '<svg width="10" height="10" viewBox="0 0 256 256" fill="currentColor"><path d="M205.66,194.34a8,8,0,0,1-11.32,11.32L128,139.31,61.66,205.66a8,8,0,0,1-11.32-11.32L116.69,128,50.34,61.66A8,8,0,0,1,61.66,50.34L128,116.69l66.34-66.35a8,8,0,0,1,11.32,11.32L139.31,128Z"/></svg>';
     rm.addEventListener("click", e => {
       e.stopPropagation();
       pendingImages.splice(i, 1);
