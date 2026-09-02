f9dfc71db42deb9c103717491d37465a0dc90f8f
VggxYT-i-use-arch-btw <viniciusgamergomes0123@gmail.com>
2026-09-02 18:17:48 -0300

    Update backend-config.js

--- backend-config.js
+++ backend-config.js
@@ -1,3 +1,3 @@
 // Cloudflared
 // Tunnel
-globalThis.BOREAS_BACKEND_URL = "https://value-prayer-licenses-fisher.trycloudflare.com";
+globalThis.BOREAS_BACKEND_URL = "https://corps-variety-expertise-engineering.trycloudflare.com";
