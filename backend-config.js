cc4992a4904be9af7188464910833c5b8862e481
VggxYT-i-use-arch-btw <viniciusgamergomes0123@gmail.com>
2026-09-02 12:53:06 -0300

    Update BOREAS_BACKEND_URL for backend configuration

--- backend-config.js
+++ backend-config.js
@@ -1,3 +1,3 @@
 // Cloudflared
 // Tunnel
-globalThis.BOREAS_BACKEND_URL = "https://registrar-wishlist-please-bosnia.trycloudflare.com";
+globalThis.BOREAS_BACKEND_URL = "https://value-prayer-licenses-fisher.trycloudflare.com";
