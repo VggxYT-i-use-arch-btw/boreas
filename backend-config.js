4ff6c892ae3157cba0754197f379c2d022705111
VggxYT-i-use-arch-btw <viniciusgamergomes0123@gmail.com>
2026-09-19 15:02:04 -0300

    Update BOREAS_BACKEND_URL for backend configuration

--- backend-config.js
+++ backend-config.js
@@ -1,3 +1,3 @@
 // Cloudflared
 // Tunnel
-globalThis.BOREAS_BACKEND_URL = "https://brooks-surfing-tamil-uncertainty.trycloudflare.com";
+globalThis.BOREAS_BACKEND_URL = "https://excitement-background-matters-rough.trycloudflare.com";
