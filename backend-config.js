510099ae914f11136658a97be6b01524fc5e3565
VggxYT-i-use-arch-btw <viniciusgamergomes0123@gmail.com>
2026-09-19 20:48:23 -0300

    Update BOREAS_BACKEND_URL for backend configuration

--- backend-config.js
+++ backend-config.js
@@ -1,3 +1,3 @@
 // Cloudflared
 // Tunnel
-globalThis.BOREAS_BACKEND_URL = "https://fields-accounting-influenced-tutorial.trycloudflare.com";
+globalThis.BOREAS_BACKEND_URL = "https://maximum-lions-disciplines-auto.trycloudflare.com";
